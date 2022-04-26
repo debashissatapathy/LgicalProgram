a9d3729e3205d7963f07ff822dd0154ece3f3dc4
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-26 11:37:37 +0530

    Fetched

---
+++ CouponNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalProgram
+{
+    class CouponNumber
+    {
+        public void couponNumber()
+        {
+
+            Console.WriteLine("Enter number of coupon");
+            int number = int.Parse(Console.ReadLine());
+            string coupon = string.Empty;
+            while (number>0)
+            {
+                int k = new Random().Next(1,123);
+                coupon += (char)k;
+                number--;
+
+            }
+       
+            Console.WriteLine("Your coupon number is: " + coupon);
+
+        }
+       
+            
+            
+        
+    }
+}
