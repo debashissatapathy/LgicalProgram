98a786d2c1bf006157a42af17cce59cf3bfa86a1
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-26 11:36:06 +0530

    Fetched

---
+++ ReverseNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalProgram
+{
+    class ReverseNumber
+    {
+        public  void Reverse_Number()
+        {
+            int n, reverse = 0, rem;
+            Console.WriteLine("Enter a number: ");
+            n = int.Parse(Console.ReadLine());
+            while (n != 0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n /= 10;
+            }
+            Console.WriteLine(reverse + " is reverse order");
+
+
+
+        }
+    }
+}
