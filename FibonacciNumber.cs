9fdd296e5673bffe2a4a36db08473db5a325983f
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-26 11:30:09 +0530

    Fetched

---
+++ FibonacciNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalProgram
+{
+    class FibonacciNumber
+    {
+        public  void Fibonacci()
+        {
+            int a = 0, b = 1, c, i, number;
+            Console.Write("enter the number of elements: ");
+            number = int.Parse(Console.ReadLine());
+            Console.Write(a + " " + b + " ");
+            for (i = 2; i < number; ++i)
+            {
+                c = a + b;
+                Console.Write(c + " ");
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
