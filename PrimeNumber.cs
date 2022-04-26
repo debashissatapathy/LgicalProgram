ae047d8bab5dc0d1228ef23c2a3b29c72749a42a
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-26 11:34:20 +0530

    Fetched

---
+++ PrimeNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalProgram
+{
+    class PrimeNumber
+    {
+        public  void Prime_Number()
+        {
+            int m, n, i, flag=0;
+            Console.WriteLine("Enter a number to find prime or not");
+            n = int.Parse(Console.ReadLine());
+            m = n / 2;
+            for(i=2; i<=m; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine("Number is not prime");
+                    flag = 1;
+                    break;
+                }
+                
+                   
+            }
+            if (flag == 0)
+                Console.WriteLine("NUmber is prime");
+
+        }
+    }
+}
